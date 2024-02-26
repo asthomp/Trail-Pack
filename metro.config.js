02def45abc5f4c11b516760befe54727da1891a9
Aaron <aaron@lastoryx.com>
2024-02-26 14:47:12 -0800

    Fixed linting errors - want all objects to have alphabetically ordered properties.

--- metro.config.js
+++ metro.config.js
@@ -2,8 +2,6 @@
 // Learn more https://docs.expo.io/guides/customizing-metro
 const { getDefaultConfig } = require("expo/metro-config");
 
-const __dirname = "/";
-
 /** @type {import('expo/metro-config').MetroConfig} */
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push("cjs");
